af1c6e36e3d6298bc6d8d5114be815f017684a4f
Vitaliy Bull <14792518+senior-bull@users.noreply.github.com>
2022-03-10 09:32:03 +0400

    Smoothie map test

---
+++ src/main/java/com/bull/algo/map/SmoothieMapTest.java
@@ -0,0 +1,25 @@
+package com.bull.algo.map;
+
+import io.timeandspace.smoothie.SmoothieMap;
+
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SmoothieMapTest {
+
+    /*
+    SmoothieMap is a Map implementation for Java with the lowest memory usage and absence of rehash latency spikes.
+    Under the hood, it is a version of extendible hashing with 48-slot mini segments.
+    */
+
+    public static void main(String[] args) {
+
+        Map<String, String> myMap = SmoothieMap.<String, String>newBuilder().build();
+
+        for (int i = 0; i < 50; i++) {
+            myMap.put(String.valueOf(ThreadLocalRandom.current().nextInt()), "vbvv");
+        }
+
+        System.out.println(myMap.size());
+    }
+}
