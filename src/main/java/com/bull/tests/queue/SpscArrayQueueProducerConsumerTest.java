d0d13a67de1d5a41ee9991ee996f88931214bfe6
Vitaliy Bull <14792518+senior-bull@users.noreply.github.com>
2022-03-19 09:47:53 +0400

    Add tests for 1-1 queue

---
+++ src/main/java/com/bull/tests/queue/SpscArrayQueueProducerConsumerTest.java
@@ -0,0 +1,39 @@
+package com.bull.tests.queue;
+
+import org.jctools.queues.SpscArrayQueue;
+import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.Arbiter;
+import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.State;
+import org.openjdk.jcstress.infra.results.I_Result;
+
+import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
+import static org.openjdk.jcstress.annotations.Expect.FORBIDDEN;
+
+@JCStressTest
+@Outcome(id = "1", expect = ACCEPTABLE, desc = "All ok.")
+@Outcome(expect = FORBIDDEN)
+@State
+public class SpscArrayQueueProducerConsumerTest {
+    private final SpscArrayQueue<Integer> queue = new SpscArrayQueue<>(3);
+
+    public SpscArrayQueueProducerConsumerTest() {
+        queue.offer(1);
+    }
+
+    @Actor
+    public void actor1() {
+        queue.poll();
+    }
+
+    @Actor
+    public void actor2() {
+        queue.offer(8);
+    }
+
+    @Arbiter
+    public void arbiter(I_Result r) {
+        r.r1 = queue.size();
+    }
+}
